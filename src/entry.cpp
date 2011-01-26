18e55ce29d685a209b6b53c644617952628f31fe
Adaptee <adaptee@gmail.com>
2011-01-26 20:15:57 +0800

    add more methos

--- src/entry.cpp
+++ src/entry.cpp
@@ -1,8 +1,17 @@
 #include "entry.h"
 
-Entry::Entry(const wstring & name): m_name(name)
+Entry::Entry(const wstring & name):
+m_name(name),
+m_parent(NULL)
 {
 
 }
+
+void
+Entry::addBlock( FileBlock * block)
+{
+    m_fileblocks.push_back(block);
+}
+
 
 
--- src/entry.h
+++ src/entry.h
@@ -3,19 +3,32 @@
 
 #include <string>
 using std::wstring;
+#include <vector>
+using std::vector;
+
+class FileBlock;
 
 class Entry
 {
 public:
     Entry(const wstring & name);
 
-    wstring name() const { return m_name; }
+    wstring name()   const { return m_name; }
+    Entry * parent() const { return m_parent; }
+    void setParent(const Entry * parent) { m_parent = parent; }
+
+    void addBlock( FileBlock * block);
+
 
     virtual bool isFile() const =0 ;
     virtual bool isDir()  const =0 ;
+    virtual size_t size() const =0 ;
 
 protected:
     wstring m_name;
+    Entry * m_parent;
+    vector<FileBlock *> m_fileblocks;
+
 };
 
 #endif /* end of include guard: ENTRY_GUARD */
