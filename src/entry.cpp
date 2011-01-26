4623342ad021d1fce13d6384dfe933695a8a9477
Adaptee <adaptee@gmail.com>
2011-01-27 00:19:46 +0800

    add sanity check

--- src/entry.cpp
+++ src/entry.cpp
@@ -1,4 +1,7 @@
+#include <assert.h>
+
 #include "entry.h"
+#include "fileblock.h"
 
 Entry::Entry(const wstring & name):
 m_name(name),
@@ -10,6 +13,10 @@ m_parent(NULL)
 void
 Entry::addBlock( FileBlock * block)
 {
+    // you do not want to add blocks belongint to others!
+    if ( m_blocks.size() > 0 )
+        assert( block->filename() == m_blocks.back()->filename() );
+
     m_blocks.push_back(block);
 }
 
