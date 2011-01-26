07aa906879256678a8dec891ad6979f0dfb627d2
Adaptee <adaptee@gmail.com>
2011-01-26 22:32:48 +0800

    add defs.h, for varisou typedefs

---
+++ src/defs.h
@@ -0,0 +1,9 @@
+#ifndef DEFS_GUARD
+#define DEFS_GUARD
+
+typedef unsigned char       byte   ;
+typedef unsigned short      uint16 ;
+typedef unsigned long       uint32 ;
+typedef unsigned long long  uint64 ;
+
+#endif /* end of include guard: DEFS_GUARD */
