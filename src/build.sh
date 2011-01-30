c6d2d64ddd51660421899b232744c43701e6609c
Adaptee <adaptee@gmail.com>
2011-01-30 10:59:27 +0800

    add unrarlib.c into build script

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-g++ -D_FILE_OFFSET_BITS=64 -g -lfuse test.cpp *block*.cpp filename.cpp archive.cpp *entry*.cpp fs.cpp
+gcc -c unrarlib.c
+g++ -D_FILE_OFFSET_BITS=64 -g -lfuse test.cpp fs.cpp archive.cpp *entry*.cpp *block*.cpp filename.cpp unrarlib.o
