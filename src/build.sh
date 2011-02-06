6e6e8f783c7a23a161f5b5a29c2a4c4d78929e84
Adaptee <adaptee@gmail.com>
2011-02-06 23:45:20 +0800

    link against libdecompress.so

--- src/build.sh
+++ src/build.sh
@@ -2,4 +2,11 @@
 
 #gcc -c unrarlib.c
 
-g++ -D_FILE_OFFSET_BITS=64 -g -lfuse test.cpp fs.cpp archive.cpp *entry*.cpp *block*.cpp descriptor.cpp filename.cpp dostime.cpp
+g++ -g -D_FILE_OFFSET_BITS=64\
+    -I  ../../unrar \
+    -L  ../../unrar \
+    -lfuse \
+    -ldecompress \
+    test.cpp fs.cpp archive.cpp *entry*.cpp *block*.cpp descriptor.cpp filename.cpp dostime.cpp
+
+    #../../unrar/libdecompress.so \
