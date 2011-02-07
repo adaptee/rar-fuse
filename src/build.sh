58f1c72204247b36a52bc37a3c080da44fce0836
Adaptee <adaptee@gmail.com>
2011-02-07 12:59:55 +0800

    rename test.cpp to main.cpp

--- src/build.sh
+++ src/build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-#gcc -c unrarlib.c
-
 g++ -g -D_FILE_OFFSET_BITS=64\
     -I  ../../unrar \
     -L  ../../unrar \
     -lfuse \
     -ldecompress \
-    test.cpp fs.cpp archive.cpp *entry*.cpp *block*.cpp descriptor.cpp filename.cpp dostime.cpp
+    main.cpp fs.cpp archive.cpp *entry*.cpp *block*.cpp descriptor.cpp filename.cpp dostime.cpp
 
     #../../unrar/libdecompress.so \
